87f7fe8b4f6c29b06debaba8d8f1bf287ede78bb
Mike Fairhurst <mfairhurst@google.com>
2019-07-10 16:37:06 -0700

    new custom background, remove sublayouts

--- xmonad.hs
+++ xmonad.hs
@@ -5,9 +5,9 @@ import XMonad.Layout.Grid
 import XMonad.Hooks.FadeInactive
 import XMonad.Hooks.Script
 import XMonad.Hooks.SetWMName
+import XMonad.Layout.Dishes
 import XMonad.Layout.Spiral
 import XMonad.Layout.Spacing
-import XMonad.Layout.SubLayouts
 import Data.Map (fromList)
 import Data.Monoid (mappend)
 import XMonad.Layout.NoBorders
@@ -24,14 +24,11 @@ myStartupHook =
   (setWMName "LG3D") -- Required for IntelliJ to work
 
 myLayout = windowNavigation
-    $ subLayout [] subThirds
     $ onWorkspace "1:terms" (thespiral ||| Full ||| gridLayout)
-    $ tiled ||| Full ||| thespiral ||| Circle ||| gridLayout
+    $ tiled ||| Full ||| thespiral ||| Circle ||| gridLayout ||| Dishes 2 (1/6)
   where
     -- one master window, half screen, and 3/100ths resize interval
     tiled = Tall 1 (3/100) (1/2)
-    -- sublayout, one master at 2/3rds, unresizable
-    subThirds = Tall 1 0 (1/3)
     -- spiral layout
     thespiral = spiralWithDir South CW (4/7)
     -- fancy spaced grid
@@ -62,14 +59,6 @@ main = xmonad $ defaultConfig {
       , ((controlMask, xK_t), spawn "clock")
       -- , ((controlMask, xK_F1), spawn "/home/mike/bin/mikehelp")
       , ((mod1Mask .|. controlMask, xK_l), spawn "slock")
-      , ((mod1Mask .|. controlMask, xK_h), sendMessage $ pullGroup L)
-      -- , ((mod1Mask .|. controlMask, xK_l), sendMessage $ pullGroup R)
-      , ((mod1Mask .|. controlMask, xK_k), sendMessage $ pullGroup U)
-      , ((mod1Mask .|. controlMask, xK_j), sendMessage $ pullGroup D)
-      , ((mod1Mask .|. controlMask, xK_m), withFocused (sendMessage . MergeAll))
-      , ((mod1Mask .|. controlMask, xK_u), withFocused (sendMessage . UnMerge))
-      , ((mod1Mask .|. controlMask, xK_period), focusUp)
-      , ((mod1Mask .|. controlMask, xK_comma), focusDown)
       , ((0, xK_Print), spawn "gnome-screenshot -i")
       ]
 
